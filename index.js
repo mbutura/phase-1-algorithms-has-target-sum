4deea73b4169ffae471b85ea72c5a24f3e19a7b5
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2022-12-16 19:00:54 +0000

    complete

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 function hasTargetSum(array, target) {
+  if(array.length == 1){
+    return false;
+  }
   // Write your algorithm here
+  for(let idx=0; idx < array.length - 1; ++idx){
+    for(let idy=idx + 1; idy < array.length; ++idy){
+      if (array[idx]+array[idy] === target){
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
 */
+
+// O(n2)
 
 /* 
   Add your pseudocode here
@@ -13,6 +26,7 @@ function hasTargetSum(array, target) {
 /*
   Add written explanation of your solution here
 */
+//start from the right end and add to those on left. Do this for each index until you reach 1
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
